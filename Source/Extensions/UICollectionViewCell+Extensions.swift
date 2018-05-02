d17c60bf3fc8c3bded98a4578d2a8fc731b45aa8
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-05-02 12:32:36 +0200

    Remove extensions

--- Source/Extensions/UICollectionViewCell+Extensions.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-extension UICollectionViewCell {
-  static var reuseIdentifier: String {
-    return String(describing: self)
-  }
-}
--- Source/Extensions/UITableViewCell+Extensions.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-extension UITableViewCell {
-  static var reuseIdentifier: String {
-    return String(describing: self)
-  }
-}
